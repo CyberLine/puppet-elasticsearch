bd48bdb351c2654ff732756a6ee32dd3dd0b553f
Alexander Over <alexander.over@check24.de>
2022-10-10 05:07:22 +0200

    fix generation of puppet types

--- lib/puppet/provider/elastic_parsedfile.rb
+++ lib/puppet/provider/elastic_parsedfile.rb
@@ -1,14 +1,26 @@
-# frozen_string_literal: true
-
 require 'puppet/provider/parsedfile'
 
 # Parent class for Elasticsearch-based providers that need to access
 # specific configuration directories.
 class Puppet::Provider::ElasticParsedFile < Puppet::Provider::ParsedFile
+  # Find/set a shield configuration file.
+  #
+  # @return String
+  def self.shield_config(val)
+    self.default_target ||= "/etc/elasticsearch/shield/#{val}"
+  end
+
   # Find/set an x-pack configuration file.
   #
   # @return String
   def self.xpack_config(val)
+    self.default_target ||= "/etc/elasticsearch/#{val}"
+  end
+
+  # Find/set an oss x-pack configuration file.
+  #
+  # @return String
+  def self.oss_xpack_config(val)
     self.default_target ||= "/etc/elasticsearch/#{val}"
   end
 end
